85fc017be86351bbb699217e805a9527b0e98cca
Eugene Yakubovich <eyakubovich@gmail.com>
2023-05-12 13:38:29 -0700

    Namespace cloud & kube labels

--- src/agent/label.rs
+++ src/agent/label.rs
@@ -1,10 +1,18 @@
+// Global namespace for most generic/common labels
 pub const LABEL_INSTANCE_ID: &str = "instance-id";
 pub const LABEL_IMAGE_ID: &str = "image-id";
 pub const LABEL_IMAGE_TAG: &str = "image-tag";
-pub const LABEL_REGION: &str = "region";
-pub const LABEL_ZONE: &str = "zone";
-pub const LABEL_CLOUD: &str = "cloud";
-pub const LABEL_ACCOUNT_ID: &str = "account-id";
-pub const LABEL_PROJECT_ID: &str = "project-id";
-pub const LABEL_KUBE_POD_NAME: &str = "kube-pod-name";
-pub const LABEL_KUBE_NAMESPACE: &str = "kube-namespace";
+
+// Cloud namespace (cloud)
+pub const LABEL_CLOUD_PROVIDER: &str = "cloud:provider";
+pub const LABEL_CLOUD_REGION: &str = "cloud:region";
+pub const LABEL_CLOUD_ZONE: &str = "cloud:zone";
+pub const LABEL_CLOUD_ACCOUNT_ID: &str = "cloud:account-id";
+pub const LABEL_CLOUD_PROJECT_ID: &str = "cloud:project-id";
+
+// Kube namespace (kube)
+// General structure:
+//   kube:<resource>:name (e.g. kube:pod:name)
+//   kube:<resource>:labels:<label> (e.g. kube:pod:labels:app.kubernetes.io/managed-by)
+pub const LABEL_KUBE_POD_NAME: &str = "kube:pod:name";
+pub const LABEL_KUBE_NAMESPACE_NAME: &str = "kube:namespace:name";
